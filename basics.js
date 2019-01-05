a8495657e046803b382782fd261166515ddd5a2a
JacobMGEvans <jacobmgevans@gmail.com>
2019-01-04 18:14:54 -0800

    basics cheatsheet +> the start of it

---
+++ basics.js
@@ -0,0 +1,21 @@
+// Comments are written inside of //
+// var, let , const keywords that are used to define variables in JavaScript
+// variables are things holding data
+var person = 'Jacob';
+let animal = 'Dog';
+const shelter = 'House';
+
+// Types in JavaScript
+null; // null is nothing, no data. no primitive type
+undefined; // undefined is somewhat self explanitory its a thing not defined
+String; // Strings are something like text
+Number; // 64bit float or integer
+Array; // a data structure that holds other data types
+Object; // Objects and Symbols are a little more complicated.
+Symbol; //
+Boolean; // simple one its either true or false
+
+// if else -> simple logic control
+// for -> looping logic
+// while -> another kind of looping logic
+//
