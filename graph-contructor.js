c51cc0d90892798200ea18131c38ece7e7b0ac6f
Jacob Evans <cloud887@gmail.com>
2019-05-25 16:11:26 -0500

    Graph Constructor

---
+++ graph-contructor.js
@@ -0,0 +1,67 @@
+import { getEnabledCategories } from "trace_events";
+
+const createNode = key => {
+  const neighbors = [];
+
+  return {
+    key,
+    neighbors,
+    addNeighbor(node) {
+      neighbors.push(node);
+    },
+  };
+};
+
+const createGraph = (directed = false) => {
+  const nodes = [];
+  const edges = [];
+  return {
+    directed,
+    nodes,
+    edges,
+
+    addNode(key) {
+      nodes.push(createNode(key));
+    },
+
+    getNode(key) {
+      return nodes.find(node => node.key === key);
+    },
+
+    addEdge(node1key, node2key) {
+      const node1 = this.getNode(node1key);
+      const node2 = this.getNode(node2key);
+
+      node1.addNeighbor(node2);
+      edges.push(`${node1key}-${node2key}`);
+
+      if (!directed) {
+        node2.addNeighbor(node1);
+      }
+    },
+    print() {
+      return nodes
+        .map(({ key, neighbors }) => {
+          let result = key;
+
+          if (neighbors.length) {
+            result += ` => ${neighbors
+              .map(neighbor => neighbor.key)
+              .join(` `)}`;
+          }
+          return result;
+        })
+        .join(`\n`);
+    },
+  };
+};
+const graph = createGraph(true);
+graph.addNode(`Jacob`);
+graph.addNode(`Child`);
+graph.addNode(`Partner`);
+graph.addNode(`Takun`);
+graph.addEdge(`Jacob`, `Partner`);
+graph.addEdge(`Jacob`, `Child`);
+graph.addEdge(`Partner`, `Takun`);
+graph.addEdge(`Child`, `Takun`);
+console.log(graph.print);
--- test.js
+++ test.js
@@ -1,7 +1,4 @@
-const age = 18;
-
-age < 18
-  ? console.log(`Sorry`)
-  : age > 18
-    ? console.log(`Power`)
-    : console.log(`Congrats first year`);
+const something = () => {
+  const thing = 12;
+};
+something();
