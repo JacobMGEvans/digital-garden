cb2a58ee8ec89f651c40ea65350ac6b0dc92669e
Jacob Evans <cloud887@gmail.com>
2019-05-25 17:25:56 -0500

    depth first search -- use yarn babel-node to run

--- graph-contructor.js
+++ graph-contructor.js
@@ -1,4 +1,4 @@
-import { getEnabledCategories } from "trace_events";
+import createQueue from "./queue-data-structure";
 
 const createNode = key => {
   const neighbors = [];
@@ -53,41 +53,81 @@ const createGraph = (directed = false) => {
         })
         .join(`\n`);
     },
+
+    // Breadth First Search
+    breadthFirstSearch(startingNodeKey, visitFn) {
+      const startingNode = this.getNode(startingNodeKey);
+      const visited = nodes.reduce((acc, node) => {
+        acc[node.key] = false;
+        return acc;
+      }, {});
+      const queue = createQueue();
+      queue.enqueue(startingNode);
+
+      while (!queue.isEmpty()) {
+        const currentNode = queue.dequeue();
+
+        if (!visited[currentNode.key]) {
+          visitFn(currentNode);
+          visited[currentNode.key] = true;
+        }
+
+        currentNode.neighbors.forEach(node => {
+          if (!visited[node.key]) {
+            queue.enqueue(node);
+          }
+        });
+      }
+    },
+
+    // Depth First Search
+    depthFirstSearch(startingNodeKey, visitFn) {
+      const startingNode = this.getNode(startingNodeKey);
+      const visited = nodes.reduce((acc, node) => {
+        acc[node.key] = false;
+        return acc;
+      }, {});
+
+      function explore(node) {
+        if (visited[node.key]) {
+          return;
+        }
+        visitFn(node);
+        visited[node.key] = true;
+        node.neighbors.forEach(node => explore(node));
+      }
+      explore(startingNode);
+    },
   };
 };
+
+//! depthfirst search dummy data - works!
 const graph = createGraph(true);
-graph.addNode(`Jacob`);
-graph.addNode(`Child`);
-graph.addNode(`Partner`);
-graph.addNode(`Takun`);
-graph.addEdge(`Jacob`, `Partner`);
-graph.addEdge(`Jacob`, `Child`);
-graph.addEdge(`Partner`, `Takun`);
-graph.addEdge(`Child`, `Takun`);
-console.log(graph.print());
+const nodes = [`a`, `b`, `c`, `d`, `e`, `f`];
+const edges = [
+  [`a`, `b`],
+  [`a`, `e`],
+  [`a`, `f`],
+  [`b`, `d`],
+  [`b`, `e`],
+  [`c`, `b`],
+  [`d`, `c`],
+  [`d`, `e`],
+];
+nodes.forEach(node => graph.addNode(node));
+edges.forEach(allNodes => graph.addEdge(...allNodes));
+graph.depthFirstSearch(`a`, node => {
+  console.log(node.key);
+});
 
-// Breadth First Search
-function breadthFirstSearch(startingNodeKey, visitFN) {
-  const startingNode = this.getNode(startingNodeKey);
-  const visited = nodes.reduce((acc, node) => {
-    acc[node.key] = false;
-    return acc;
-  }, {});
-  const queue = createQueue();
-  queue.enqueue(startingNode);
-
-  while (!queue.isEmpty()) {
-    const currentNode = queue.dequeue();
-
-    if (!visited[currentNode.key]) {
-      visitFn(currentNode);
-      visited[currentNode.key] = true;
-    }
-
-    currentNode.neighbors.forEach(node => {
-      if (!visited[node.key]) {
-        queue.enqueue(node);
-      }
-    });
-  }
-}
+//! Checking if the graph factory - works!
+// const graph = createGraph(true);
+// graph.addNode(`Jacob`);
+// graph.addNode(`Child`);
+// graph.addNode(`Partner`);
+// graph.addNode(`Takun`);
+// graph.addEdge(`Jacob`, `Partner`);
+// graph.addEdge(`Jacob`, `Child`);
+// graph.addEdge(`Partner`, `Takun`);
+// graph.addEdge(`Child`, `Takun`);
+// console.log(graph.print());
--- queue-data-structure.js
+++ queue-data-structure.js
@@ -1,5 +1,4 @@
-// queue
-
+// queue data structure => function factory
 export function createQueue() {
   const queue = [];
   return {
@@ -22,6 +21,37 @@ export function createQueue() {
     // isempty
     isEmpty() {
       return queue.length === 0;
+    },
+  };
+}
+// Priority Queue
+export function createPriorityQueue() {
+  const lowPriorityQueue = createQueue();
+  const highPriorityQueue = createQueue();
+
+  return {
+    enqueue(item, isHighPriority = false) {
+      isHighPriority
+        ? highPriorityQueue.enqueue(item)
+        : lowPriorityQueue.enqueue(item);
+    },
+    dequeue() {
+      if (!highPriorityQueue.isEmpty()) {
+        return highPriorityQueue.dequeue();
+      }
+      return lowPriorityQueue.dequeue();
+    },
+    peek() {
+      if (!highPriorityQueue.isEmpty()) {
+        return highPriorityQueue.peek();
+      }
+      return lowPriorityQueue.peek();
+    },
+    length() {
+      return highPriorityQueue.length + lowPriorityQueue.length;
+    },
+    isEmpty() {
+      return highPriorityQueue.isEmpty() && lowPriorityQueue.isEmpty();
     },
   };
 }
