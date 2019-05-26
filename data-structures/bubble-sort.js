450d60a4155e1095942cca2a7efb021e84e43e75
Jacob Evans <cloud887@gmail.com>
2019-05-26 16:39:05 -0500

    modulize and cleanup

---
+++ data-structures/bubble-sort.js
@@ -0,0 +1,26 @@
+// bubble sort
+
+// Loop through the array
+// if this item > next item, swap them
+// indicate a swap occured
+// if a swap occured, loop through the array again
+// continue looping until no swaps occur
+export function bubbleSort(array) {
+  let swapped = false;
+  do {
+    swapped = false;
+
+    array.forEach((element, index) => {
+      if (element > array[index + 1]) {
+        const temporary = element;
+        array[index] = array[index + 1];
+        array[index + 1] = temporary;
+        swapped = true;
+      }
+    });
+  } while (swapped);
+  return array;
+}
+
+const numbers = [10, 5, 6, 3, 2, 8, 9, 4, 7, 1];
+bubbleSort(numbers);
