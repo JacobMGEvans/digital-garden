38a4c495b2c639fb19cd5370446d8764da8ef600
Jacob Evans <cloud887@gmail.com>
2019-07-04 17:43:13 -0500

    ANAGRAM WORKS

--- testing/experimental-field.js
+++ testing/experimental-field.js
@@ -1,13 +1,24 @@
-const string = `HELLOWORLD`;
-const memory = [];
+const TEST_STRING = `HELLOWORLD`;
 
-export const convertAnagram = str => {
-  const random = Math.random() * 10;
-  let count = 0;
-  while (count < str.length) {
-    if (random < str.length) memory.push(str.slice(random, random + 1));
-    count++;
-  }
+/**
+ * @function randomIntGenerator used to get random number for string positionrandomizing
+ * @param {Integer} ind index passed in to further pseudo randomize
+ */
+const randomIntGenerator = ind => Math.floor(ind * Math.random() * 10);
+
+/**
+ * @function convertAnagram converts a string into an anagram of itself.
+ * @param {String} str string to be transformed.
+ */
+const convertAnagram = str => {
+  const memory = [];
+  const anagramify = (ele, ind) =>
+    randomIntGenerator(ind) % 2 ? memory.push(ele) : memory.unshift(ele);
+
+  Array.from(str).map(anagramify);
+  return memory.join(``);
 };
-console.log(memory);
-console.log(convertAnagram(string));
+
+console.log(convertAnagram(TEST_STRING));
+
+module.export = convertAnagram;
--- testing/testing-field.test.js
+++ testing/testing-field.test.js
@@ -1,1 +1,9 @@
-import { convertAnagram } from "./experimental-field.js";
+import { convertAnagram } from "./experimental-field";
+
+describe(`experimental-coding modules`, async () => {
+  test(`should recieve string and convert it to an anagram with no side effects`, async () => {
+    const TEST_STRING = `HELLOWORLD`;
+    const returnedString = convertAnagram(TEST_STRING);
+    expect(returnedString !== TEST_STRING);
+  });
+});
