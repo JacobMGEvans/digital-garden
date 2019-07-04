89cdc2b7f2a981d6c939768ad179d073f353486b
Jacob Evans <cloud887@gmail.com>
2019-07-04 15:30:10 -0500

    anagram testing field

--- testing/experimental-field.js
+++ testing/experimental-field.js
@@ -1,3 +1,13 @@
-const weird = Array(5);
+const string = `HELLOWORLD`;
+const memory = [];
 
-console.log(weird[0]);
+export const convertAnagram = str => {
+  const random = Math.random() * 10;
+  let count = 0;
+  while (count < str.length) {
+    if (random < str.length) memory.push(str.slice(random, random + 1));
+    count++;
+  }
+};
+console.log(memory);
+console.log(convertAnagram(string));
---
+++ testing/testing-field.test.js
@@ -0,0 +1,1 @@
+import { convertAnagram } from "./experimental-field.js";
