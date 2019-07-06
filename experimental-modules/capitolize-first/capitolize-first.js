52f130fcd67f2e809f1ea42ba37efdb024a4ed3d
Jacob Evans <cloud887@gmail.com>
2019-07-06 11:52:43 -0500

    capitolize

--- experimental-modules/capitolize-first/capitolize-first.js
+++ experimental-modules/capitolize-first/capitolize-first.js
@@ -1,13 +1,17 @@
 const TEST_STRING = `HELLO WORLD OF PEOPLE...`;
 const TEST_STRING2 = `people are everywhere?`;
 
+/**
+ * @function capitolizeFirst - Convert sentence to uppercase all the first letters for each word in a sentence.
+ * @param {...String} String can take in sentences as seperate arguments
+ * @return {Array} Array of converted strings.
+ * @example {Input => Output} 'HeLlO WOrLd' => 'Hello World'
+ */
 const capitolizeFirst = (...args) => {
-  /**
-   * @arrStr
-   *  - array of string passed into the function
-   */
-  const arrStr = [...args];
+  const arrStr = [...[...args]];
   console.log(arrStr);
   const mappedArr = arrStr.map(ele => ele);
 };
 capitolizeFirst(TEST_STRING, TEST_STRING2);
+
+export { capitolizeFirst };
