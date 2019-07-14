07137fb0e355644eefe89a4f4d0ea422a6da243b
Jacob Evans <cloud887@gmail.com>
2019-07-13 23:22:51 -0500

    misc and babel upgrades

--- experimental-modules/misc/index.js
+++ experimental-modules/misc/index.js
@@ -1,5 +1,7 @@
 const oldArray = [1, 2, 3, 4, 5];
 const otherData = [`Jacob`];
+const animalNames = { cat: `furball`, dog: `sirbarksalot` };
 
-const newArray = [...oldArray, ...otherData, `cat`];
-console.log(newArray);
+const pointFreeDoubleInt = (ele) => Number.isInteger(ele) ? ele * 12 : ele;
+const newArray = [...oldArray, ...otherData, ...Object.values(animalNames)].map(pointFreeDoubleInt);
+console.log(newArray); // [ 12, 24, 36, 48, 60, 'Jacob', 'furball', 'sirbarksalot' ]
