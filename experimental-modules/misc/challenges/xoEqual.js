cd155eb5f47beb1df00e06dd2e80dc446264140b
Jacob Evans <cloud887@gmail.com>
2019-08-05 20:37:46 -0500

    palidromeRearrange

--- experimental-modules/misc/challenges/xoEqual.js
+++ experimental-modules/misc/challenges/xoEqual.js
@@ -1,7 +1,34 @@
-function XO(s) {}
+function XO(s) {
+  const arr = [...s];
+  let countX = 0;
+  let countO = 0;
+  arr.map((ele, i) => {
+    if (ele === `x`) {
+      countX++;
+      delete arr[i];
+    }
+    if (ele === `X`) {
+      countX++;
+      delete arr[i];
+    }
+    if (ele === `o`) {
+      countO++;
+      delete arr[i];
+    }
+    if (ele === `O`) {
+      countO++;
+      delete arr[i];
+    } else {
+      delete arr[i];
+    }
+  });
+  const compare = countO === countX;
+  return compare;
+}
 
-console.log(XO("ooxx")); // true
-console.log(XO("xooxx")); // false
-console.log(XO("ooxXm")); // true
-console.log(XO("zpzpzpp")); // true // when no 'x' and 'o' is present should return true
-console.log(XO("zzoo")); // false
+console.log(XO(`xxxoooxxxoooxxxoooxoxoppppppppppppxoooxx`)); // true
+// console.log(XO(`ooxx`)); // true
+// console.log(XO(`xooxx`)); // false
+// console.log(XO(`zpzpzpp`)); // true
+// console.log(XO(`zzoo`)); // false
+// console.log(XO(`zzoOOOoo`));
