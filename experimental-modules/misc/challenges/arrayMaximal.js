32218afba619b043d4af5041ce1d9073ebdf6218
Jacob Evans <cloud887@gmail.com>
2019-08-08 21:23:21 -0500

    maximal

---
+++ experimental-modules/misc/challenges/arrayMaximal.js
@@ -0,0 +1,16 @@
+function arrayMaximalAdjacentDifference(inputArray) {
+  const max = Math.max(...inputArray);
+  const nexPlus = inputArray[inputArray.indexOf(max) + 1] || inputArray[inputArray.indexOf(max) - 1];
+
+
+  return max - nexPlus;
+}
+
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([1, 1, 1, 1]));
+
+console.log(arrayMaximalAdjacentDifference([2, 1, 15, -15]));
+// arrayMaximalAdjacentDifference([2, 4, 1, 0]);
+
+// arrayMaximalAdjacentDifference([2, 4, 1, 0]);
+// arrayMaximalAdjacentDifference([2, 4, 1, 0]);
