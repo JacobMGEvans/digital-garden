71d510fc40e000f0990516de113a011fa7059996
Jacob Evans <cloud887@gmail.com>
2019-08-17 21:37:11 -0500

    flattenDeep

--- experimental-modules/misc/flattenDeep.js
+++ experimental-modules/misc/flattenDeep.js
@@ -1,3 +1,10 @@
+// example from MDN documentaion
+
+// to enable deep level flatten use recursion with reduce and concat
+const testArray = [1, 2, 3, [1, 2, 3, 4, [2, 3, 4]]];
+
 export function flattenDeep(arr1) {
   return arr1.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
 }
+
+console.log(flattenDeep(testArray));
