739535a0109f8e7649246e1813ce7f866b3083a5
Jacob Evans <cloud887@gmail.com>
2019-11-15 17:08:59 -0600

    comment and name change TS pointfree

--- functional-programming/ts/pointfree-tacit.ts
+++ functional-programming/ts/pointfree-tacit.ts
@@ -3,14 +3,15 @@
 const tsArray: Array<number> = [1, 2, 3];
 
 //  x is an interim variable
-array.map(x => x * 2);
+// normal example
+tsArray.map(x => x * 2);
 
 /**
  * @function double example of pointfree programming
  * @param {Number} argument to be doubled
  */
 const tsDouble = (x: number): number => x * 2;
-array.map(tsDouble);
+tsArray.map(tsDouble);
 
 // legibility
 // reduce surface area for bugs
