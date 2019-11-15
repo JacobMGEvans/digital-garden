41713f6cc3d9e913201094de52273b7458dc3b81
Jacob Evans <cloud887@gmail.com>
2019-11-15 17:07:48 -0600

    higher order ts

---
+++ functional-programming/ts/higher-order.ts
@@ -0,0 +1,47 @@
+/*  Higher Order Function
+ 1. Accepts a function as an argument
+ 2. Returns a new Function */
+// Immutable Data
+// Mutable = can be changed after creation
+// immuatable = cant be changed after creation
+
+/**
+
+ * @param function
+ * @example
+ * const a = [1, 2, 3];
+ * const b = a;
+ * b.push(4);
+ * console.log(a);
+ * OUTPUT: [1, 2, 3, 4]
+ */
+export const withCount = (fn: any): any => {
+  let count = 0;
+
+  return (...args: any) => {
+    console.log(`Call count: ${++count}`);
+    return fn(...args);
+  };
+};
+// const add = (x, y) => x + y;
+// const countedAdd = withCount(add);
+// console.log(countedAdd(1, 2)); // 3
+// console.log(countedAdd(2, 2)); // 4
+
+/** A push curried function that doesn't mutate the original array
+   * @function push(Any)(Array)
+   * @type{function(Any): function(Array): Array}
+   * @return Array
+  
+   * @example const a = [1, 2, 3];
+   const b = push(4)(a);
+   console.log(a); // [1,2,3]
+   console.log(b); // [1,2,3,4]
+   */
+export const push: (arg0: any) => (arg0: any) => any = (
+  value: any
+) => array => {
+  const clone = [...array];
+  clone.push(value);
+  return clone;
+};
