8eed75ea68202f3397029aa3dd8d18eef83c9a93
Jacob Evans <cloud887@gmail.com>
2019-12-06 16:52:07 -0600

    Node line reader

--- experiments&testing-fields/fileReader.js
+++ experiments&testing-fields/fileReader.js
@@ -1,3 +1,21 @@
-const fs = require('fs')
 
-console.log(fs)
+const fs = require('fs'),
+	readline = require('readline');
+
+let lineNum = 0;
+let lineStr = '';
+
+const readInterface = readline.createInterface({
+	input: fs.createReadStream(`${__dirname}/ipsum-lorem.txt`),
+	console: true,
+	terminal: false,
+});
+
+readInterface.on('line', line => {
+	lineNum++;
+	lineStr = console.log(line);
+});
+
+// readInterface.on('close', () => {
+// 	console.log('Total lines : ' + lineNum);
+// });
