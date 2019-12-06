5ca3d2b099f73871e1a0b58c1e7309278a443fc7
Jacob Evans <cloud887@gmail.com>
2019-12-06 17:14:40 -0600

    read the text file

--- experiments&testing-fields/fileReader.js
+++ experiments&testing-fields/fileReader.js
@@ -1,21 +1,25 @@
 
 const fs = require('fs'),
-	readline = require('readline');
+      readline = require('readline');
 
 let lineNum = 0;
 let lineStr = '';
 
+
 const readInterface = readline.createInterface({
 	input: fs.createReadStream(`${__dirname}/ipsum-lorem.txt`),
 	console: true,
-	terminal: false,
+	terminal: true,
+    output: process.stderr
 });
 
 readInterface.on('line', line => {
 	lineNum++;
-	lineStr = console.log(line);
+    lineStr = line
 });
 
-// readInterface.on('close', () => {
-// 	console.log('Total lines : ' + lineNum);
-// });
+console.log(lineStr )
+readInterface.on('close', () => {
+    console.log('Total lines : ' + lineNum);
+    return lineStr
+});
