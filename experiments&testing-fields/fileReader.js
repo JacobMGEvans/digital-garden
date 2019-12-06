f7401f3d72ad7a7ad007afc805fa8f3d9266907f
Jacob Evans <cloud887@gmail.com>
2019-12-06 17:42:48 -0600

    Node file reader and adds numbers to the front of the line for each line read

--- experiments&testing-fields/fileReader.js
+++ experiments&testing-fields/fileReader.js
@@ -1,25 +1,17 @@
+const fs = require("fs"),
+  readline = require("readline");
 
-const fs = require('fs'),
-      readline = require('readline');
+// instantiate the Node file reader
+const readInterface = readline.createInterface({
+  input: fs.createReadStream(`./ipsum-lorem.txt`),
+  console: true,
+  terminal: true
+});
 
 let lineNum = 0;
-let lineStr = '';
 
-
-const readInterface = readline.createInterface({
-	input: fs.createReadStream(`${__dirname}/ipsum-lorem.txt`),
-	console: true,
-	terminal: true,
-    output: process.stderr
-});
-
-readInterface.on('line', line => {
-	lineNum++;
-    lineStr = line
-});
-
-console.log(lineStr )
-readInterface.on('close', () => {
-    console.log('Total lines : ' + lineNum);
-    return lineStr
+// This is where you can change things per line. 
+readInterface.on("line", line => {
+  lineNum++;
+  console.log(`${lineNum}   ${line}`);
 });
