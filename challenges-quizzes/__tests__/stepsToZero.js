c4ae478dbad2b3e572ec1ce32e17b2606b160d73
Jacob Evans <cloud887@gmail.com>
2020-03-29 18:15:59 -0500

    tests

--- challenges-quizzes/__tests__/stepsToZero.js
+++ challenges-quizzes/__tests__/stepsToZero.js
@@ -1,8 +1,11 @@
 import { numberOfSteps } from "../stepsToZero";
 
 test(`should resolve 14 to output 6`, () => {
-  numberOfSteps(14).toBe(6);
+  expect(numberOfSteps(14)).toBe(6);
 });
 test(`should resolve 14 to output 6`, () => {
-  numberOfSteps(10).toBe(6);
+  expect(numberOfSteps(10)).toBe(5);
+});
+test(`should resolve 14 to output 6`, () => {
+  expect(numberOfSteps(8)).toBe(4);
 });
