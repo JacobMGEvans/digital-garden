7e8f454068591b357c696cfd82d1dd48d5b0a92e
Jacob Evans <cloud887@gmail.com>
2020-04-18 00:47:19 -0500

    deno environment setup

--- server.ts
+++ server.ts
@@ -3,10 +3,12 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import { __ } from 'https://deno.land/x/dirname/mod.ts';
 const {__dirname } = __(import.meta);
 
-const PORT = process.env.PORT || 1234;
+const PORT = this.env().PORT || 1234;
 
 const server = new Application()
-server()
-  .use(server.path(`${__dirname}/dist`))
-  .get(`*`, (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
-  .listen(PORT, () => console.log(`__SERVER_RUNNING__`, PORT));
+
+await server.listen(`${PORT}`)
+// server()
+//   .use(server.path(`${__dirname}/dist`))
+//   .get(`*`, (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
+//   .listen(PORT, () => console.log(`__SERVER_RUNNING__`, PORT));
