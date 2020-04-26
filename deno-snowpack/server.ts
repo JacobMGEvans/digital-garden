d78fc0f731563a04ef5125f9520dfa3909b23c94
Jacob Evans <cloud887@gmail.com>
2020-04-26 18:23:06 -0500

    DENO server fix

--- deno-snowpack/server.ts
+++ deno-snowpack/server.ts
@@ -1,16 +1,17 @@
 // @ts-ignore
-import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://denopkg.com/Soremwar/oak@v0.41/mod.ts";
 import { __ } from 'https://deno.land/x/dirname/mod.ts';
-const {__dirname } = __(import.meta);
+// const {__dirname } = __(import.meta);
 
 // @ts-ignore
 const env = Deno.env();
 const HOST_PORT = `127.0.0.1:${env.PORT}`;
+console.log(env)
 
 const server = new Application()
-const router = new Router()
+// const router = new Router()
 // router.get(`*`)
-
+console.log(Application)
 server
   .use(async (cxt: any): Promise<any> => {
     console.log(cxt)
@@ -19,6 +20,7 @@ server
 //     index: "index.html"
 // })
 })
+
 // server.use(router.routes())
   
 
