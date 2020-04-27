1de18484dfb9ed1d384f4ce99073b264642dc28a
Jacob Evans <cloud887@gmail.com>
2020-04-27 00:09:20 -0500

    Static DENO server working

--- deno-snowpack/server.ts
+++ deno-snowpack/server.ts
@@ -1,27 +1,28 @@
 // @ts-ignore
-import { Application } from "https://denopkg.com/Soremwar/oak@v0.41/mod.ts";
+import { Application, Router, send } from "https://denopkg.com/Soremwar/oak@v0.41/mod.ts";
 import { __ } from 'https://deno.land/x/dirname/mod.ts';
-// const {__dirname } = __(import.meta);
+import "https://deno.land/x/dotenv/load.ts";
 
+const {__dirname } = __(import.meta);
+
+console.log(__dirname)
 // @ts-ignore
 const env = Deno.env();
 const HOST_PORT = `127.0.0.1:${env.PORT}`;
-console.log(env)
 
 const server = new Application()
-// const router = new Router()
-// router.get(`*`)
-console.log(Application)
+const router = new Router()
+
+router.get(`*`)
+server.use(router.routes())
 server
-  .use(async (cxt: any): Promise<any> => {
-    console.log(cxt)
-//     await send(cxt, cxt.request.path, { 
-//     root: `${__dirname}`,
-//     index: "index.html"
-// })
+  .use(async (context: any): Promise<any> => {
+    await send(context, context.request.path,{ 
+    root: `${__dirname}`,
+    index: "index.html"
+})
 })
 
-// server.use(router.routes())
   
 
 console.log(`__SERVER_RUNNING__${HOST_PORT}`)
