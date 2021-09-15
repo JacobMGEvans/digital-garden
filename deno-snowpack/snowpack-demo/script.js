a9d940a71918728634aefed86fc978f82dbeb734
Jacob M-G Evans <27247160+JacobMGEvans@users.noreply.github.com>
2021-09-15 16:29:12 -0500

    Delete Snowpack

--- deno-snowpack/snowpack-demo/script.js
+++
@@ -1,9 +0,0 @@
-const arr = [`Foo`, `Bar`, `World`];
-
-const bodyNode = document.body;
-const newNode = document.createElement(`div`);
-const breakNode = document.createElement(`br`);
-arr.map((ele) => {
-  bodyNode.appendChild(newNode).appendChild(breakNode);
-  newNode.append(ele);
-});
