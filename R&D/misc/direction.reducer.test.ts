e2becc075936d04c2ca63f59a2e8b2035b704823
JacobMGEvans <jacobmgevans@gmail.com>
2023-05-07 14:16:02 -0500

    TS convert

--- R&D/misc/direction.reducer.test.ts
+++ R&D/misc/direction.reducer.test.ts
@@ -4,4 +4,5 @@ import { dirReduc } from './direction.reducer'
 test('test dirReduc', () => {
   expect(dirReduc([])).toEqual({ NORTH: 0, EAST: 0, SOUTH: 0, WEST: 0 });
   expect(dirReduc([`NORTH`, `SOUTH`, `SOUTH`, `EAST`, `WEST`, `NORTH`, `WEST`])).toEqual({ NORTH: 1, EAST: 1, SOUTH: 0, WEST: 0 });
+  expect(dirReduc([`NORTH`, `NORTH`, `NORTH`])).toEqual({ NORTH: 3, EAST: 0, SOUTH: 0, WEST: 0 });
 })
