d415343dbe4c585140dc4ddfec09463bd71c7320
JacobMGEvans <jacobmgevans@gmail.com>
2023-05-07 13:48:48 -0500

    TS convert

---
+++ data-structures/merge-sort.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from 'vitest'
+import { mergeSort } from './merge-sort'
+
+test('test test-field.mergeSort', () => {
+  expect(mergeSort([2, 1, 3])).toEqual([1, 2, 3]);
+  expect(mergeSort(['f', 'a', 'b', 'c', 'b', 'z'])).toEqual(['a', 'b', 'b', 'c', 'f', 'z']);
+
+  const randomNumArray = Array.from({ length: 100 }, () => Math.floor(Math.random() * 100))
+  expect(mergeSort(randomNumArray)).toEqual(randomNumArray.sort((a, b) => a - b));
+  // log the speed of the mergeSort function vs the built in sort function
+  console.time('mergeSort');
+  mergeSort(randomNumArray);
+  console.timeEnd('mergeSort');
+  console.time('built-in sort');
+  randomNumArray.sort((a, b) => a - b);
+  console.timeEnd('built-in sort');
+
+})
--- data-structures/merge-sort.ts
+++ data-structures/merge-sort.ts
@@ -2,31 +2,30 @@
 
 // Divides the given array into two halves: a left and right sub array
 // Calls mergeSort on these sub array
-// Once we get to arrays that are less than 2 in length we begin to stitch them up thus leading to or sorted array by the time its all the way back up the stack
-export function merge(left, right) {
-  const sorted = [];
+// Once we get to arrays that are less than 2 in length we begin to stitch them up thus leading to or sorted array 
+// by the time its all the way back up the stack
+export function merge<T>(left: T[], right: T[]): T[] {
+  const sorted: T[] = [];
   while (left.length && right.length) {
     if (left[0] <= right[0]) {
-      sorted.push(left.shift());
+      sorted.push(left.shift()!);
     } else {
-      sorted.push(right.shift());
+      sorted.push(right.shift()!);
     }
   }
-
-  const results = [...sorted, ...left, ...right];
-  console.log(results);
-  return results;
+  return [...sorted, ...left, ...right];
 }
 
-export function mergeSort(array) {
+// This mergeSort function is a recursive function that will continue to break down the array into left and right sub arrays until they are less than 2 in length
+// The generics here are used to ensure that the array passed in is of the same type as the array that is returned
+export function mergeSort<T>(array: T[]): T[] {
   if (array.length < 2) {
     return array;
   }
   const middle = Math.floor(array.length / 2);
   const left = array.slice(0, middle);
   const right = array.slice(middle);
-
-  return merge(mergeSort(left), mergeSort(right));
+  return merge<T>(mergeSort(left), mergeSort(right));
 }
 
 // //! mergeSort
