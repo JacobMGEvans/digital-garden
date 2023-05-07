646b8e8c339ea0db53a6e19b842acc17d17623b5
JacobMGEvans <jacobmgevans@gmail.com>
2023-05-07 12:11:30 -0500

    convert to TS

--- challenges-quizzes/arrayMaximal.ts
+++ challenges-quizzes/arrayMaximal.ts
@@ -1,10 +1,10 @@
-function arrayMaximalAdjacentDifference(inputArray) {
+function arrayMaximalAdjacentDifference(inputArray: number[]): number {
   const max = Math.max(...inputArray);
   const nexPlus = inputArray[inputArray.indexOf(max) + 1] || inputArray[inputArray.indexOf(max) - 1];
 
-
   return max - nexPlus;
 }
+
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
 console.log(arrayMaximalAdjacentDifference([1, 1, 1, 1]));
