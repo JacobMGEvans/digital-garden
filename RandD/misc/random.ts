c4dbbbdcbbbe1000d09f3b822228f08de6873fbf
JacobMGEvans <jacobmgevans@gmail.com>
2023-05-07 14:47:39 -0500

    TS convert

--- RandD/misc/random.ts
+++ RandD/misc/random.ts
@@ -1,15 +1,15 @@
-const arr = [1, 1, 1, 1, 3, 4, 4, 4, 1234, 1234, 1233];
+const numberArr = [1, 1, 1, 1, 3, 4, 4, 4, 1234, 1234, 1233];
 const obj = { dog: `bark`, cat: `bark`, cow: `moo` };
-const deDuplicate = input => [...new Set(input)];
+const deDuplicate = (input: Array<number>) => [...new Set(input)];
 
-console.log(deDuplicate(arr)); // [ 1, 3, 4, 1234, 1233 ]
+console.log(deDuplicate(numberArr)); // [ 1, 3, 4, 1234, 1233 ]
 
 const mySet = new Set([1, 2, 1223, 431242, 342345245235, 2123123, 12313]);
 
 // check if prime
 // check if even
 // check if multiple of 10
-const number = evenNum => multipleOfTen => primeEvenTen => [
+const number = (evenNum: number) => (multipleOfTen: number) => (primeEvenTen: number) => [
   evenNum % 2 === 0,
   multipleOfTen % 10 === 0,
   Number.isInteger(primeEvenTen / 3),
@@ -17,5 +17,4 @@ const number = evenNum => multipleOfTen => primeEvenTen => [
 console.log(number(100)(100)(100));
 
 const xhr = new XMLHttpRequest();
-
 
